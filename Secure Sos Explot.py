f3534b51030794ad2f858fb2823d6440eef7b4f7
SwaritMisro12 <95137718+SwaritMisro12@users.noreply.github.com>
2022-12-04 15:15:08 +0530

    Add files via upload

---
+++ Secure Sos Explot.py
@@ -0,0 +1,363 @@
+
+#To Import Required modules for This Project
+import  pyttsx
+import twiiter.api.sys
+import hyerkepy
+import setupofcase
+import macthtime
+import fustupcust
+import AiinLen
+import PowerHostu
+import enc.host 
+import credits.enter
+import saneai
+import IPChangesRroo
+
+
+#to Define A System as local to a user
+__init__(system.local)
+             def system.local(len{system}):
+                int = a (
+                    <1.2>
+                   )
+                int = b ( <-> local.int(in.<"\nSystem\nResult">)
+                     <2,1>
+                 (Makes.local ) local <-> )
+
+#used some maths for the range loop of local = defined for sin,cos
+int = sin.<local> = makes{in.sys}
+ sin,cos = 0% <=> for a in range(12%): = cos
+       else (b) = sin
+  sin = 1
+  cos =1
+
+#The defined to max as input and min as output
+__init_def__(max.input)
+       int max.input{"a"}
+       else 
+ __init_def__(min.output)
+        int min.output{"b"}     
+           elif
+   = true.system[followed.up.commands]
+    max.true if system("allowes")<=> else 
+        int = c("0") 
+        scopes.mac.followed{spefic.lack.local} <-> else 
+        int = d("0")     
+        elif
+ = true.system[followed.up.commands]
+    min.false if system("denyes")<=> else 
+        int = e("0") 
+        scopes.mac.followed{spefic.lack.local} <-> else 
+        int = f("0")
+             return;
+
+
+
+# this for the local value in form of sin,cos sin = rep as allow and cos = rep as declined
+if system = {local.value} 
+        intput.followed(spefic.string"allowes") <=>
+          return;    
+          else
+    int output.value = {sin.cos}
+          elif
+
+            if string.sin(root.algined["3/2"])<=>
+               value = root.["3/2"]
+             return;
+                elif
+         if string.cos(root.algined["3/2"])<=>
+            value = root.["3/2"]
+            return;
+            
+  #this for the form of sin and cos in rootstatment
+def rootstatement("sin,cos")
+    if sin.cos <=> {algined.statement(root."sin = 3/2",root."cos = 3/2")}
+     return;
+
+   #this is used for local as root method for sin,cos
+ int rootstatement <++> algined.locals{"Worked"}
+   else 
+ int rootstatement <--> error.defined("System Error") 
+   return;
+
+  
+#this for query form method
+__init__def___(<//>a;)
+                  system = if string{"spefic"}
+                scopes.<system.value>
+                abort.<len.lenght("%22")>
+                int abort.sys = {start/make.<"Forcast/usr/den/clean">}
+            if abort.sys[<=>clean.make =system.error]
+              max try[defined.output]
+              int o = defined.output = value.rep{cort.makeup.query}
+              def query({?}) <=> 
+                elif 
+                if query.thing = macthtime{"Ai?"}
+                      return;
+
+   #this for Ai(New Method)
+int Ai("Auvalue")
+      def query.Ai{value.rep}
+      int Ai = query.rep
+      let.local(0)
+      sys.intput range {["defined"]}
+      range output = value{"\nQuery\nType\nDeafult"} section.query 
+      equals int a + sys.len(abort.allow) = local.system
+      return;
+
+#this for the host how is hosting as there instance for different users
+class host 
+int PhU(string.allow+__init_def__)
+     if PhU.commands.followed = value.cos 
+      int a,b = (root.sin.cos="3/2")     
+      elif
+      def a.b + root.host = system?PhU("Required_Api")  
+      return;
+
+#host entrance 
+in.host("Entered")
+int maxFins = len in (system.locals) = rep.host = desktop.allow set max value<-=$$$$$$$$$=->
+      if encrypted.host(value.local)= enc.rep 
+          root.value . set max value intput range (<header.0>)
+          <=>
+      else  
+      int minFins  = query.enc*host("UPARDE PLEASE SYSTEM HANDLE CRASH")
+        section.abort _ root("Abort Handle CRASH").restart
+           sector^restart = value + len
+           int from sys.stop(:"Hanlder Crash":)
+           return;
+
+
+in.host("Error Crash handler")
+int handler.crash =sys.work  = Deafult.lenght("error")
+   return;
+
+in.host("Stopped and Exited")
+   int exit("Exited")
+      if value = stop.exit 
+         Phu.stop 
+         return; 
+
+
+int system.settings*("Setting Menu")
+          if string.value(cos.algined)
+           system.of <Ai>followed = output.input {Turbo.boost}
+           system.abort 
+                allow.abort all 
+                 sys.ai = while + Loop + Local<=>{?}
+                    ai.turbo:loop:turbo:local<_Ai_>
+                    while 
+                    system.exit = trubo
+                return; 
+
+int system.menu(root.sys)
+        int Password,Email,ID
+        if Email = root.allow <=>
+         enter.abort = new.task("Bar")
+         Bar.allow for a in range :Allow:
+         locals
+            elif 
+        if Password = root.allow <-> 
+            abort.defined = sys.<int + locals>  
+            all.enter = defined.true if bool = false    
+            max = <"1">
+            min <"1">
+            elif 
+          if ID = defined.value in int  = sin.cos 
+             value = defined.root all 
+             sector.allow =locals + <:"Abort":>
+             ID = id = {local.host}
+              section = try if id.allow 
+              {"**********************"}
+              return;
+             
+                 
+#Anticheat to not get easliy for ip adderes and it is change every time
+int anticheat("Sifu") 
+   if Sifu = rep.ai 
+      try = trubo 
+       max = sys.allow 
+       min = sys.abort 
+    services.max = file.compile("Cleared") 
+      while 
+      min = header.turbo{"allowed"}
+    for 
+    max = defined.root = stop.allow<=>abort.encrypted{"declined"} 
+    return;
+
+int services(0)
+      int 0 = max 
+       int 0 = min 
+        services.header(root.allow)   
+        system.abort = file.specfic = try 
+         while sys =("Under Connections")
+         return;
+
+
+
+int main = defined (system)
+   def system = local sys
+      then sys = mac.point = all 
+      allowed
+      return;
+int system.Sifu("AntiCheat")
+    if system = root.rep in range("10")
+       abort.int(output.range)
+       all locals = all variables
+       return;
+
+int abort.sys("Old System(1.5)")
+           if abort.sys = exit in system
+               all locals allowed + try while system
+                  exit.stop 
+                  return;
+
+
+
+ int abort.system("New System(Update_2.0)")                 
+                if system.value = defined("Local is Defined") in system(locals = value)
+                    in system = value(self) = {local.maxFins}
+                              max = system.value
+                              else
+                              min = system.value
+                              return;
+
+def @event.client("Main")
+      if main = @event.client("Local User")
+           in @event.client = defined.all{user.locals}
+           return;
+
+
+
+int @event.client("Json".rep)
+      if system = variables.(allowed)
+         rep.json("Local")
+         if system.root = allowed(.rep{Up-lo})
+          <=> _____________________
+              _ Root(id) = ""     _
+              _ Email(id) = ""    _   
+              _ Password(id) = "" _
+              _ IPLocal(id) = ""  _
+              ______________________
+                return;
+#to Secure our connections so Working on Great method and even adding Sub Modules for Anticheat (Sifu)
+in Sifu()
+  int Sifu("New Modules")
+     if @event.client() = rep.all 
+        users.acess()
+        print("AntiCheat had New Update for Secure Connections")
+          acess.local <-=->
+        <=> acess.a
+        int a = (IP.id)
+           acess.id = new.all
+       int b = (IP.cha) <=>
+           acess.b = new.all
+           int add(done Button)
+              if system.id = ("allowed")
+              print("Thanks for this Modules To Make Setup for this better Network")
+           return;
+
+int IpMain(Ip_Exist_Moduels)
+       if main.local = host(.id) <--> trigger("If You Have Some Problems Then Use This Specailly Made Modules")
+             def value = id.location("char.values")
+                 else 
+           def value = char.location("Main.Values")
+           return;
+
+int IpMain(Ip_Exist_Moduels.allow)
+    if main.local = location.ip("Has A Local Area Of IpvDns")
+                  in system <.> range = rootstatement("Make a different statement")
+                while("1,10")
+                   in allow(0)
+                   case.settings = set max value
+                      in range Loop("Class")
+                      return;
+
+
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
